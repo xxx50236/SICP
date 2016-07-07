f1cd51e3250bd830e7d434276e8d1c864459d99d
cb <437046579@qq.com>
2016-07-07 22:22:10 +0800

    Make Change Orders of Growth

---
+++ Exercise-1.14.scm
@@ -0,0 +1,7 @@
+;;About this exercis, I don't know how to figure it out.
+;;And I find some resource about this exercise
+
+
+
+;;The URl is https://github.com/jiacai2050/sicp/blob/master/exercises/01/1.14.md
+;;
